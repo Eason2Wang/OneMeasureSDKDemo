8cf5bb74de955befc5c216aeb89c82b82be6cdea
wang1song <wys839348916@gmail.com>
2019-01-25 14:11:05 +0800

    sdk 1.1.6

--- app/src/main/java/com/tozmart/tozisdkdemo/MainActivity.java
+++ app/src/main/java/com/tozmart/tozisdkdemo/MainActivity.java
@@ -2,6 +2,12 @@ package com.tozmart.tozisdkdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.tozmart.tozisdk.app.ToziSDK;
+import com.tozmart.tozisdk.constant.Gender;
+import com.tozmart.tozisdk.constant.Language;
+import com.tozmart.tozisdk.constant.Unit;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.start).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new ToziSDK.Builder()
+                        .setBlurFace(false)
+                        .setCorpId("o4x04hy3zljghf7oa4")
+                        .setGender(Gender.MALE)
+                        .setHeight(180)
+                        .setWeight(70)
+                        .setLanguage(Language.CHINESE)
+                        .setName("wys")
+                        .setUnit(Unit.METRIC)
+                        .start(MainActivity.this);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/tozmart/tozisdkdemo/MyApplication.java
@@ -0,0 +1,36 @@
+package com.tozmart.tozisdkdemo;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.jess.arms.base.App;
+import com.jess.arms.di.component.AppComponent;
+import com.tozmart.tozisdk.app.ToziSDK;
+
+public class MyApplication extends Application implements App {
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        ToziSDK.getInstance().attachBaseContext(base);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        ToziSDK.getInstance().onCreate(this);
+    }
+
+    /**
+     * 在模拟环境中程序终止时会被调用
+     */
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+        ToziSDK.getInstance().onTerminate(this);
+    }
+
+    @Override
+    public AppComponent getAppComponent() {
+        return ((App) ToziSDK.getInstance().getmAppDelegate()).getAppComponent();
+    }
+}
